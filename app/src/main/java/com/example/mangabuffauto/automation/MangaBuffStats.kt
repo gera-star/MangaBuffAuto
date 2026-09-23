ed861b22b37efaabdbd0f84cb170de22abdd8eb4
Gera <gerain123123@gmail.com>
2026-09-23 21:39:23 +0900

    Add Android source project files

---
+++ app/src/main/java/com/example/mangabuffauto/automation/MangaBuffStats.kt
@@ -0,0 +1,12 @@
+package com.example.mangabuffauto.automation
+
+data class MangaBuffStats(
+    val diamonds: Int? = null,
+    val mineOre: Int? = null,
+    val cardsCurrent: Int? = null,
+    val cardsMax: Int? = null,
+    val adsCurrent: Int? = null,
+    val adsMax: Int? = null,
+    val commentsCurrent: Int? = null,
+    val commentsMax: Int? = null
+)
